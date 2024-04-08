665804c90791b880dd670325381f02f6a88cc70a
frmuller41182 <frankmullerat@gmail.com>
2024-04-09 00:10:54 +0200

    Progress on 4-9-2024 1. Successfully rendered a not so pretty first drfat of the index HTML page with HTML paragraphs stacked together that have the question field of the question object. 2. Successfully tested runnign the script with 50 and 100 different questions. NEXT STEPS 1. Biggest task to tacke now is how we can generate a new HTML file for each of the questions and add a link with an anchor tag to the different paragraphs.

--- frank-p2-ts-final/main.ts
+++ frank-p2-ts-final/main.ts
@@ -1,5 +1,11 @@
-//We first import the function that fetches the data from the
+//We first import the function that fetches the data from the trivia API
 import { getQuestions } from "./questions";
+import { writeFile } from "fs/promises";
+import { header } from "./render";
+import { draftPage } from "./render";
 
 //Main program which leverages the functions and scripts we created in seperate files and are importing here.
-const questions = await getQuestions(10);
+const questions = await getQuestions(100);
+const html = draftPage(questions);
+
+await writeFile("index.html", html);
---
+++ frank-p2-ts-final/render.ts
@@ -0,0 +1,61 @@
+/*this file will contain the function to render the objects (questions) of my program.
+ */
+import { Question } from "./questions";
+
+export const header = () => {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="A diverse, merit-based educational institution, Virtus Scientia Academy is dedicated to fostering excellence and 
+      virtue in the pursuit of knowledge. Offering unparalleled education with a focus on innovation, 
+      leadership, and philosophy, we empower students to achieve their highest potential in a global society."
+    />
+    <meta
+      name="keywords"
+      content="educational, academy, leadership, stoicism, global, innovation, wisdom, college, technology, economics, politics, "
+    />
+    <link
+      rel="icon"
+      type="image/ico"
+      href="https://a0.awsstatic.com/libra-css/images/site/fav/favicon.ico"
+    />
+    <link rel="stylesheet" href="VSA.css" />
+    <title>Virtus Scientia Academy (VSA) | Home Page</title>
+  </head>`;
+};
+
+export const draftPage = (questions: Array<Question>) => {
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="A diverse, merit-based educational institution, Virtus Scientia Academy is dedicated to fostering excellence and 
+        virtue in the pursuit of knowledge. Offering unparalleled education with a focus on innovation, 
+        leadership, and philosophy, we empower students to achieve their highest potential in a global society."
+      />
+      <meta
+        name="keywords"
+        content="educational, academy, leadership, stoicism, global, innovation, wisdom, college, technology, economics, politics, "
+      />
+      <link
+        rel="icon"
+        type="image/ico"
+        href="https://a0.awsstatic.com/libra-css/images/site/fav/favicon.ico"
+      />
+      <link rel="stylesheet" href="VSA.css" />
+      <title>Virtus Scientia Academy (VSA) | Home Page</title>
+    </head>
+    <body>`;
+  for (const question of questions) {
+    html += `<p> ${question.question}</p>`;
+  }
+  html += "</body>";
+  return html;
+};
